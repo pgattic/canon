2c0930e394d798d911b8a1592781d08209a7b968
pgattic <pgattic@gmail.com>
2024-02-09 13:53:22 -0700

    Distinguish reference parsing code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
 
 	//"regexp"
@@ -10,21 +9,10 @@ import (
   "strconv"
 )
 
-
-func main() {
-  args := os.Args
-
-  if len(args) < 2 {
-    fmt.Println("Please specify an output.")
-    return
-  }
-
-  var reference string = args[1]
-
+func parse_reference(reference string) (book string, chapter string, start_verse int, end_verse int) {
   splat := strings.Split(reference, ":")
-  book := splat[0]
+  book = splat[0]
   ref_str := splat[1]
-  var chapter string
 
   for strings.Contains("1234567890", book[len(book)-1:]) { // Separate the chapter number from book name
     chapter = string(book[len(book)-1]) + chapter
@@ -39,18 +27,39 @@ func main() {
     end_ref_str = split[1]
   }
 
-//  fmt.Println(book)
-//  fmt.Println(chapter)
-//  fmt.Println(ref_str)
-//  fmt.Println(end_ref_str)
+  start_verse, err_1 := strconv.Atoi(ref_str)
+
+  if err_1 != nil {
+    fmt.Println("Dang it")
+    return
+  }
+
+  var err_2 error
+
+  if end_ref_str != "" {
+    end_verse, err_2 = strconv.Atoi(end_ref_str)
+    if err_2 != nil {
+      fmt.Println("Dang it")
+    }
+  } else {
+    end_verse = start_verse
+  }
+  return book, chapter, start_verse, end_verse
+}
+
+func main() {
+  args := os.Args
+
+  if len(args) < 2 {
+    fmt.Println("Please specify an output.")
+    return
+  }
+
+  var reference string = args[1]
+
+  book, chapter, start_verse, end_verse := parse_reference(reference)
 
   fs_ref := "texts/Book of Mormon/" + book + "/" + chapter + ".md"
-
-  fmt.Println(fs_ref)
-
-  if fs.ValidPath(fs_ref) { // this is useless and does nothing
-    fmt.Println("yes")
-  }
 
   dat, err := os.ReadFile(fs_ref)
 
@@ -60,53 +69,9 @@ func main() {
 
   chap := strings.Split(string(dat), "\n")
 
-
-  ref, err_1 := strconv.Atoi(ref_str)
-
-  if err_1 != nil {
-    fmt.Println("Dang it")
-    return
-  }
-
-  var result string
-
-  if end_ref_str != "" {
-    fmt.Println("more than one verse")
-    end_ref, err_2 := strconv.Atoi(end_ref_str)
-    if err_2 != nil {
-      fmt.Println("Dang it")
-      return
-    }
-    result = strings.Join(chap[ref-1:end_ref], "\n")
-  } else {
-    result = chap[ref-1]
-  }
+  result := strings.Join(chap[start_verse-1:end_verse], "\n")
 
   fmt.Println(result)
-
-//  pattern := `^([^:]+)(\d+):(\d+)(?:-(\d+))?$`
-//
-//  re := regexp.MustCompile(pattern)
-//  matches := re.FindStringSubmatch(input)
-//
-//  if len(matches) == 0 {
-//    fmt.Println("No match found")
-//    return
-//  }
-//
-//  name := matches[1]
-//  number1 := matches[2]
-//  number2 := matches[3]
-//  numberRange := matches[4]
-//
-//  fmt.Println("Name:", name)
-//  fmt.Println("Number 1:", number1)
-//  fmt.Println("Number 2:", number2)
-//  if numberRange != "" {
-//    fmt.Println("Number Range:", numberRange)
-//  } else {
-//    fmt.Println("No number range specified")
-//  }
 }
 
 
